e8ec66d4b10592abc4ba28245e98535aabd66388
srinivasthedeveloper <srinivasthedeveloper@gmail.com>
2021-10-01 09:05:35 +0530

    setting up commit

---
+++ Accordian/App.js
@@ -0,0 +1,28 @@
+import 'react-native-gesture-handler';
+import * as React from 'react';
+import {useEffect} from 'react';
+import {StyleSheet, SafeAreaView, Platform, StatusBar} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
+
+import HomePage from './HomePage';
+
+function App() {
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <HomePage />
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight - 100 : 0,
+  },
+});
+
+export default App;
